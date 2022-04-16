33a0148df6619ce7d788eea62bfdaa14eb6afa38
SS041 <63644018+SS041@users.noreply.github.com>
2022-04-16 13:54:55 -0700

    Create edit.js

---
+++ editFood/edit.js
@@ -0,0 +1,8 @@
+var but = document.querySelector('.add');
+but.onclick = function(){
+    alert("Added");
+}
+var but = document.querySelector('.back');
+but.onclick = function(){
+    location.href = "../menuCreation/food.html";
+}
